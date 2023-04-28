4dbb13ae9c6b08a8889ee2848bba9920e367783d
LeeHyunJin1997 <hjnee1102s@gmail.com>
2023-04-28 17:21:34 +0900

    feat: make question api - just working for get sample data

--- question_maker/question/views.py
+++ question_maker/question/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET',])
 def question(request):
-    pass
+    data = {
+        "결과": "성공!"
+    }
+    return Response(data, status=status.HTTP_200_OK)
