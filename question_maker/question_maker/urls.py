e10e14a36b950ec1a5cce958be758c254aaccb2e
LeeHyunJin1997 <hjnee1102s@gmail.com>
2023-05-25 07:56:55 +0900

    fix: ELB health check

--- question_maker/question_maker/urls.py
+++ question_maker/question_maker/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health', views.health_check, name='health_check'),
     path('api/v1/', include('question.urls'))
 ]
